5700ed7da108e16595e3aab9262b8ab92012d9e0
Macbook-pro <robertodelprete88@gmail.com>
2024-06-14 12:01:18 +0200

    setup and isntall separate

---
+++ install_bash.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# This script installs the SARLens package and its dependencies
+
+# Check if Conda is installed
+if ! command -v conda &> /dev/null
+then
+    echo "Conda is not installed. Please install Conda and try again."
+    echo "See https://docs.anaconda.com/anaconda/install/ for more information."
+    exit 1
+fi
+
+# Create a new Conda environment called AINavi
+conda create -n SARLens python=3.10 -y
+
+# Activate the AINavi environment
+source $(conda info --base)/etc/profile.d/conda.sh
+if conda activate SARLens; then
+    echo "SARLens environment activated"
+else
+    echo "SARLens environment not found"
+    exit 1
+fi
+
+# Check if the script has been sourced
+if [ -z "$BASH_SOURCE" ]; then
+    echo "ERROR: You must source this script. Run 'source $0'"
+    exit 1
+fi
+# Check if the script is being run from the right directory
+if [ ! -f setup.py ]; then
+  echo "ERROR: Run this from the top-level directory of the repository"
+  exit 1
+fi
+# Run the setup.py script
+python setup.py develop
+
+source setup.sh
